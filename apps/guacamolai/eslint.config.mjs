87de68e3c577fc3405545b0458e550a8086f6fb6
Younes Jaaidi <younes@marmicode.io>
2025-03-01 00:38:33 +0100

    test: ✅ fix e2e config

--- apps/guacamolai/eslint.config.mjs
+++ apps/guacamolai/eslint.config.mjs
@@ -1,8 +1,6 @@
-import playwright from 'eslint-plugin-playwright';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
-  playwright.configs['flat/recommended'],
   ...baseConfig,
   {
     files: ['**/*.ts', '**/*.js'],
